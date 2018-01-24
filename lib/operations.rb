a25f44614393a5093494edebc5e6fa7f5bc47381
Stephen Thurman <thurman024@gmail.com>
2018-01-24 16:38:06 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-	speed < 40 || speed > 60 ? true : false
+	if speed < 40 || speed > 60
+		return true
+	else
+		return false
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
-	
-
-
